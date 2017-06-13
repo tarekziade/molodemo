640aecf112cc45c7453e4b9b042d8cc00877a1f3
Tarek Ziade <tarek@ziade.org>
2017-06-13 11:22:14 +0200

    demo

---
+++ loadtest.py
@@ -0,0 +1,14 @@
+import molotov
+
+
+@molotov.scenario(weight=10)
+async def scenario_one(session):
+    async with session.get('http://localhost:8000') as resp:
+        assert resp.status == 200
+
+
+@molotov.scenario(weight=20)
+async def scenario_one(session):
+    async with session.get('http://localhost:8000/data.json') as resp:
+        data = await resp.json()
+        assert 'ok' in data
